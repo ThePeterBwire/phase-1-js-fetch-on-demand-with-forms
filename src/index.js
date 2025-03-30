060fe99d3c6c4cf6745b17e011d54f950819a4b4
ThePeterBwire <peterbwirea@gmail.com>
2025-03-31 02:07:55 +0300

    pushes fetchondemandforms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
+      const movieId = input.value;
+  
+      fetch(`http://localhost:3000/movies/${movieId}`)
+        .then(response => response.json())
+        .then(data => {
+          const titleElement = document.querySelector('#movieDetails h4');
+          const summaryElement = document.querySelector('#movieDetails p');
+          
+          titleElement.textContent = data.title;
+          summaryElement.textContent = data.summary;
+        });
+    });
+  };
+  
+  document.addEventListener('DOMContentLoaded', init);
